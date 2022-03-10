13ca55459b6c09223697d032e8d3192cc229aa5b
p.tamozhniy <p.tamozhniy@itcsolutions.ru>
2022-03-11 00:38:39 +0300

    cors whith Helix

--- msg-service-backend/src/index.ts
+++ msg-service-backend/src/index.ts
@@ -10,9 +10,17 @@ import {
 } from 'graphql-helix'
 import { schema } from './schema'
 import { contextFactory } from './context'
+import cors from 'fastify-cors'
 
 const main = async () => {
   const server = fastify()
+
+  server.register(cors, {
+    origin: '*',
+    // methods: 'GET, POST, PUT, DELETE, OPTIONS',
+    // allowedHeaders: 'Origin, Content-Type, Accept, Authorization',
+    // credentials: true,
+  })
 
   server.route({
     method: ['POST', 'GET'],
@@ -24,6 +32,17 @@ const main = async () => {
         query: req.query,
         body: req.body,
       }
+
+      reply.header('Access-Control-Allow-Origin', '*')
+      reply.header(
+        'Access-Control-Allow-Methods',
+        'GET, POST, PUT, DELETE, OPTIONS'
+      )
+      reply.header(
+        'Access-Control-Allow-Headers',
+        'Origin, Content-Type, Accept, Authorization'
+      )
+      reply.header('Access-Control-Allow-Credentials', 'true')
 
       if (shouldRenderGraphiQL(request)) {
         reply.header('Content-Type', 'text/html')
@@ -47,7 +66,7 @@ const main = async () => {
     },
   })
 
-  server.listen(8002, '0.0.0.0', () => {
+  server.listen(8002, 'localhost', () => {
     console.log('http://localhost:8002/graphql')
   })
 }
